27cbd500cd9d7ab5c26c9f9eb54cf6b4c135f18b
Marco Rizzo <marco.rizzo.00@outlook.com>
2020-06-15 15:28:33 +0200

    ANXCamera: add 48mp fix for IMX sensors Thanks to Noid

--- anx-vendor.mk
+++ anx-vendor.mk
@@ -6,7 +6,7 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/product/etc,$(TARGET_COPY_OUT_PRODUCT)/etc) \
     $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/product/priv-app/ANXCamera/lib,$(TARGET_COPY_OUT_PRODUCT)/priv-app/ANXCamera/lib) \
     $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/product/priv-app/ANXExtraPhoto/lib,$(TARGET_COPY_OUT_PRODUCT)/priv-app/ANXExtraPhoto/lib) \
-    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc)
+    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system,$(TARGET_COPY_OUT_SYSTEM))
 
 PRODUCT_PACKAGES += \
     ANXCamera \
